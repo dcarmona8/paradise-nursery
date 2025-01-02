34e31186b90ac7df97a622d5a194bc57b629ee21
dcarmona8 <dcarmonamd@gmail.com>
2025-01-02 22:48:34 +0100

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
-import React, { useState } from "react";
-import "./App.css";
-import ConferenceEvent from "./ConferenceEvent";
-import AboutUs from "./AboutUs";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import ProductsPage from './components/ProductsPage';
 
-function App() {
-  const [showVenue, setShowVenue] = useState(false);
-
-  const handleGetStarted = () => {
-    setShowVenue(true);
-  };
-
-  return (
-    <>
-      <header className="first_page">
-        <div className="main_event">
-          <div className="first_page_name_btn">
-            <h1 className="budget_heading">Conference Expense Planner</h1>
-            <p className="budget_sentence"> Plan your next major event with us!</p>
-            <div className="getstarted_btn">
-              <button onClick={() => handleGetStarted()} className="get-started-btn">
-                Get Started
-              </button>
-            </div>
-          </div>
-          <div className="aboutus_main">
-            <AboutUs />
-          </div>
-        </div>
-      </header>
-
-      <div className={`event-list-container ${showVenue ? 'visible' : ''}`}>
-        <ConferenceEvent />
-      </div>
-    </>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/products" element={<ProductsPage />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
